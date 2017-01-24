de8dda70e62cbd967796d91822a7c57d99d00352
Jim Youngquist <jay@cs.uw.edu>
2017-01-23 23:44:07 -0800

    GLFW window running. No content.

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,56 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main(int argc, char **argv) {
   // DELETE THESE.  Used to suppress unused variable warnings.
   (void)argc;
   (void)argv;
 
-  std::cout << "Hello, World!" << std::endl;
+  if (!glfwInit())
+  {
+      return -1;
+  }
 
+  glfwSetErrorCallback(error_callback);
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "oglMidi", NULL, NULL);
+  if (!window)
+  {
+      // Window or OpenGL context creation failed
+  }
+  glfwMakeContextCurrent(window);
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+  glfwSetKeyCallback(window, key_callback);
+
+  std::cout << "oglMidi running..." << std::endl;
+
+  glfwSwapInterval(1);
+
+  while (!glfwWindowShouldClose(window))
+  {
+      glfwPollEvents();
+
+      // Keep running
+      int width, height;
+      glfwGetFramebufferSize(window, &width, &height);
+      glViewport(0, 0, width, height);
+
+      glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
   return 0;
 }
